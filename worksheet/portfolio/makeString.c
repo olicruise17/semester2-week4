70b82d4f5df781137a3474fb3a853b4e191aa95b
Oli Cruise <cruiseoli@outlook.com>
2026-02-25 16:13:07 +0000

    Worksheet complete

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Oliver Cruise
+Student ID: 201823825
 */
 
 #include <stdio.h>
@@ -11,7 +11,18 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
 
+    char buffer[10] = {0};
+
     // process the command-line data using appropriate string functions
+
+    
+
+    for ( int k=1; k<argc; k++) {
+        strcat(buffer, argv[k]);
+        if (k < argc - 1) {
+            strcat(buffer,"-");
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
